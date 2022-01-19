dab0ef84ae266489a1cd1a86e0c5b4a4fe721e75
KYJTHEYJ <nananan1213@gmail.com>
2022-01-20 00:48:44 +0900

    20220120

---
+++ Kotlin/01. 변수/Work.kts
@@ -0,0 +1,19 @@
+// 1. var 변수 선언
+var changeYes = 100;
+
+//Kotlin 에서는 변수의 생성과 초기화(할당)이 한번에 이루어져야 한다
+//var changeYes;
+//changeYes = 100;
+
+println(changeYes);
+
+changeYes = 200;
+println(changeYes);
+
+// 2. val 변수 선언
+val CHANGE_NO = 100;
+println(CHANGE_NO);
+//CHANGE_NO = 200; // val은 재할당이 불가능함
+
+val SCHOOL_NAME = "학교";
+println(SCHOOL_NAME);
